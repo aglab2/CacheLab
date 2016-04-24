e80756e2d5c30aa9892afec7301508f69926c4f8
aglab2 <denis.kopyrin@phystech.edu>
2016-04-24 14:11:24 +0300

    Fixed var names and added struct for cache for better readability

--- csim.c
+++ csim.c
@@ -3,10 +3,16 @@
 #include <getopt.h>
 #include <stdio.h>
 
+struct cache_el{
+	long long used;
+	long long tag;
+	long long lru_counter;
+};
+
 int s = 0, E = 0, b = 0, verbosity = 0;
 int S = 0, B = 0;
 int hit_count = 0, miss_count = 0, eviction_count = 0;
-long long ** cache = NULL;
+struct cache_el ** cache = NULL;
 long long set_index_mask = 0;
 long long lru_counter = 0;
 
@@ -41,20 +47,20 @@ int main(int argc, char * argv[])
 				return 1;
 		}
 	}
-	
+
 	if (s == 0 || E == 0 || b == 0 || trace_file == 0){
 		printf("%s: Отсутствует необходимый аргумент командной строки\n", argv[0]);
 		printUsage(argv[0]);
 		return 1;
 	}
-	
+
 	S = 1 << s;
 	B = 1 << b;
-	
+
 	initCache();
 	replayTrace(trace_file);
 	freeCache();
-	
+
     printSummary(hit_count, miss_count, eviction_count);
     return 0;
 }
@@ -76,42 +82,43 @@ void printUsage(char * name){
 void initCache(){
 	int i;
 	cache = malloc(S * sizeof(*cache));
-	for (i=0; i<S; i++) cache[i] = calloc(3 * E, sizeof(*cache[i]));
+	for (i=0; i<S; i++) cache[i] = calloc(E, sizeof(*cache[i]));
 
 	set_index_mask = S - 1;
 }
 
 void accessData(long long address){
 	int i;
-	long long Eaddress = (address >> b) & set_index_mask; //rbp30
-	long long Caddress = (address >> (b+s)); //rbp28
-	
+	long long set_num = (address >> b) & set_index_mask; //rbp30
+	long long tag = (address >> (b+s)); //rbp28
+
 	for (i=0; i<E; i++){
-		if (cache[Eaddress][3*i+1] == Caddress && cache[Eaddress][3*i] != 0){
+		if (cache[set_num][i].tag == tag && cache[set_num][i].used != 0){
 			hit_count++;
 			if (verbosity) printf("hit ");
-			cache[Eaddress][3*i+2] = lru_counter++;
+			cache[set_num][i].lru_counter = lru_counter++;
 			return;
 		}
 	}
 
 	miss_count++;
-	long long minLru = 0xffffffff;
-	int EvicI = 0;
+	long long min_lru = 0xffffffff;
+	int evicted_line = 0;
 	if (verbosity) printf("miss ");
-	for (i=0; i<E; i++)
-		if (cache[Eaddress][3*i+2] < minLru){
-			EvicI = i;
-			minLru = cache[Eaddress][3*i+2];
+	for (i=0; i<E; i++){
+		if (cache[set_num][i].lru_counter < min_lru){
+			evicted_line = i;
+			min_lru = cache[set_num][i].lru_counter;
 		}
-	
-	if (cache[Eaddress][3*EvicI] != 0){
+	}
+
+	if (cache[set_num][evicted_line].used == 1){
 		eviction_count++;
 		if (verbosity) printf("eviction ");
 	}
-	cache[Eaddress][3*EvicI] = 1;
-	cache[Eaddress][3*EvicI+1] = Caddress;
-	cache[Eaddress][3*EvicI+2] = lru_counter++;
+	cache[set_num][evicted_line].used = 1;
+	cache[set_num][evicted_line].tag = tag;
+	cache[set_num][evicted_line].lru_counter = lru_counter++;
 }
 
 void replayTrace(char * trace_file){
@@ -121,13 +128,13 @@ void replayTrace(char * trace_file){
 		printf("%s: No such file or directory!", trace_file);
 		return;
 	}
-	
+
 	while(fgets(buf, 1000, fp)){
 		unsigned long long address = 0;
 		unsigned length = 0;
 		if (buf[1] == 'S' || buf[1] == 'L' || buf[1] == 'M'){
 			sscanf(buf+2, "%llx,%u", &address , &length);
-			
+
 			if (verbosity) printf("%c %llx,%u ", buf[1], address, length);
 			accessData(address);
 		}
